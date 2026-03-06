25970c717bcae99778467d37be8df558a2963866
Nina K <20558616+pinkcloudsnina@users.noreply.github.com>
2026-03-06 16:51:04 +0200

    init: start online zoo business logic task

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import {defineConfig} from 'eslint/config';
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        extends: ['js/recommended'],
+        languageOptions: {
+            globals: globals.browser,
+        },
+    },
+
+    {
+        files: ['**/*.{ts,mts,cts}'],
+        extends: ['plugin:@typescript-eslint/recommended'],
+        languageOptions: {
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+            globals: globals.browser,
+        },
+    },
+]);
